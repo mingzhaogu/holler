6c93f79975910aa65d3e0c33c8819c04ade2cf20
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-03 15:00:02 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+usernames = ['lilopelekai', 'stitch626', 'pleakley', 'evilgeniusjumba']
+
+image_urls = [
+  "https://freeclipartimage.com//storage/upload/lilo-and-stitch-clip-art/lilo-and-stitch-clip-art-7.gif",
+  "https://uploads.scratch.mit.edu/users/avatars/1361/0949.png",
+  "https://2.bp.blogspot.com/-CtNhmCNtIqI/VsDY9PxwlLI/AAAAAAAACVo/u50tvlqikJA/s1600/pleakley12.jpg",
+  "https://vignette.wikia.nocookie.net/stitchipediaalilostitch/images/f/f2/Jumba.gif/revision/latest/scale-to-width-down/350?cb=20110516163835"
+]
+
+first_names = ['Lilo', 'Stitch', 'Wendy', 'Jumba']
+last_names = ['Pelekai', '626', 'Pleakley', 'Jookiba']
+
+4.times do |i|
+  User.create(
+    username: usernames[i],
+    password: 'password',
+    image_url: image_urls[i],
+    first_name: first_names[i],
+    last_name: last_names[i]
+  )
+end
