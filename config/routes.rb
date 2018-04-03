e0a36241e6c90155bb70850f81a36241673f0279
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-03 15:02:03 -0700

    Configure gems, routes, webpack

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  namespace :api do
+  namespace :api, defaults: {format: :json} do
     resources :user, only: [:new, :create]
     resource :session, only: [:new, :create, :destroy]
   end
