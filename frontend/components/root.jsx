9f43f87e87e4b8eb8b9e8c366a36ef88b7127794
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-03 17:21:29 -0700

    Render <App>

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
+import App from './app'
+
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
-      <h1>HARROOOOOOOO, when ready, replace this with opencarrot-App-space-slash-closecarrow</h1>
+      <App />
     </HashRouter>
   </Provider>
 );
