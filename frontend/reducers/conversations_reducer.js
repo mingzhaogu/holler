f5a11e473eec9b46f5c2033416785d172b5e6b1d
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-05 11:07:50 -0700

    Create conversation reducer

---
+++ frontend/reducers/conversations_reducer.js
@@ -0,0 +1,28 @@
+import merge from 'lodash';
+import {
+  RECEIVE_ALL_CONVERSATIONS,
+  RECEIVE_CONVERSATION,
+  // REMOVE_CONVERSATION
+} from '../actions/conversation_actions';
+
+const ConversationReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  let newState;
+
+  switch(action.type) {
+    case RECEIVE_ALL_CONVERSATIONS:
+      return merge({}, action.posts);
+    case RECEIVE_CONVERSATION:
+      const receivedConvo = { [action.conversation.id]: action.conversation };
+      newState = merge({}, oldState, receivedConvo);
+      return newState;
+    // case REMOVE_CONVERSATION:
+    //   newState - merge({}, oldState);
+    //   delete newState[action.conversationId];
+    //   return newState;
+    default:
+      return oldState;
+  }
+};
+
+export default ConversationReducer;
