afb797c0d511f44e8004d25cfbcb6d48e7b8a796
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-05 11:09:24 -0700

    Add conversations reducer to entities

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,11 +1,11 @@
-// import { combineReducers } from 'redux';
-//
-// // import each individual reducer:
-// import session from './session_reducer';
-//
-// // fix rootReducer to include actual reducers
-// const entitiesReducer = combineReducers({
-//   session,
-// });
-//
-// export default entitiesReducer;
+import { combineReducers } from 'redux';
+
+// import each individual reducer:
+import conversations from '/conversations_reducer';
+
+// fix rootReducer to include actual reducers
+const entitiesReducer = combineReducers({
+  conversations,
+});
+
+export default entitiesReducer;
