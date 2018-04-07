2acedd9813d27c3f4817be26c8883461e222f93a
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-06 23:45:20 -0700

    Start ActionCable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,18 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user =
+    end
+
+    private
+
+    def find_current_user
+      if current_user = User.find_by(session[:session_token])
+        current_user
+      else
+        reject_unauthorized_connection
+      end
   end
 end
