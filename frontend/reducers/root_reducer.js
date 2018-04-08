e39f4ed8c10fb8b8bda2015decba347647ec2987
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-07 17:55:54 -0700

    Add UI Reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -4,12 +4,14 @@ import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import ErrorsReducer from './errors_reducer';
 import EntitiesReducer from './entities_reducer';
+import UIReducer from './ui_reducer';
 
 // fix rootReducer to include actual reducers
 const rootReducer = combineReducers({
   entities: EntitiesReducer,
   session: SessionReducer,
-  errors: ErrorsReducer
+  errors: ErrorsReducer,
+  ui: UIReducer
 });
 
 export default rootReducer;
---
+++ frontend/reducers/ui_reducer.js
@@ -0,0 +1,23 @@
+import { merge } from 'lodash';
+import {
+  TOGGLE_FRIEND_DETAILS_VIEW,
+} from '../actions/ui_actions';
+
+const _nullState = {
+  showFriendDetails: false
+};
+
+const UIReducer = (oldState = _nullState, action) => {
+  Object.freeze (oldState);
+
+  let newState;
+  switch(action.type) {
+    case TOGGLE_FRIEND_DETAILS_VIEW:
+      newState = merge({}, { showFriendDetails: !oldState.showFriendDetails});
+      return newState;
+    default:
+      return oldState;
+  }
+};
+
+export default UIReducer;
