dc2b39980eb5ea90d3a01779415dfdc18e7ab606
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-07 23:48:28 -0700

    Include actioncable routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
     end
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static_pages#root"
 end
