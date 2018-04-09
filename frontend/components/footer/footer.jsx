25ef39e9c09a6a994a0e96180358e7271a6b43df
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-09 00:35:35 -0700

    Refactor icon links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { GithubIconLink, LinkedInIconLink } from './icon_links';
 
 const Footer = () => {
-
   return (
     <footer className="footer">
-
-      <a href="https://www.linkedin.com/in/mingzhaogu/"
-        target="_blank">
-        <i className="fa fa-linkedin-square"
-          aria-hidden="true"
-        />
-      </a>
-
-      <a href="https://github.com/mingzhaogu"
-        target="_blank">
-        <i className="fa fa-github-square"
-          aria-hidden="true"
-        />
-      </a>
-
+      <GithubIconLink />
+      <LinkedInIconLink />
     </footer>
   )
 }
