745cee014f751ffba89c1d4491480890d912f4de
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-09 16:17:07 -0700

    Stream from correct channel

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,24 +1,9 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    debugger
-    stream_from "chat-2"
+    stream_from params[:room]
   end
 
   def unsubscribed
     stop_all_streams
   end
-
-  private
-
-  def channel_name
-    debugger
-    "chat-#{params[:id]}"
-  end
-
-  # def format_response(data)
-  #   {
-  #     body: data["body"],
-  #     username: current_user.username
-  #   }
-  # end
 end
