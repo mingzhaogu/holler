46f0bcf1373935e118c2a694e3d8068742b5fe71
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-09 00:35:22 -0700

    Factor out github and linkin icon links

---
+++ frontend/components/footer/icon_links.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const GithubIconLink = () => (
+  <a href="https://www.linkedin.com/in/mingzhaogu/"
+    target="_blank">
+    <i className="fa fa-linkedin-square"
+      aria-hidden="true"
+    />
+  </a>
+)
+
+export const LinkedInIconLink = () => (
+  <a href="https://github.com/mingzhaogu"
+    target="_blank">
+    <i className="fa fa-github-square"
+      aria-hidden="true"
+    />
+  </a>
+)
