113c30241385d12aa7b6b45b60610578e2d24a47
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-08 23:26:48 -0700

    Change sessions render

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -8,7 +8,7 @@ class Api::SessionsController < ApplicationController
 
     if @user
       login(@user)
-      render 'api/users/show'
+      render 'api/sessions/create'
     else
       render json: ["Invalid credentials"], status: 401
     end
