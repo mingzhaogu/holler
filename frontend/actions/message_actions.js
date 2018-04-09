0b24dfe12c6e8f38b5e16fc02a709794d2470dcd
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-09 16:27:08 -0700

    Create FetchMessages action

--- frontend/actions/message_actions.js
+++ frontend/actions/message_actions.js
@@ -2,18 +2,29 @@ import * as MessageUtil from '../util/message_api_util';
 
 export const SET_MESSAGES = 'SET_MESSAGES';
 export const RECEIVE_MESSAGE = 'RECEIVE_MESSAGE';
+// export const RECEIVE_MESSAGES = 'RECEIVE_MESSAGES';
 
 export const setMessages = (messages) => ({
   type: SET_MESSAGES,
   messages
 });
 
-export const receiveMessage = (message) => ({
+export const receiveMessage = (payload) => ({
   type: RECEIVE_MESSAGE,
-  message
+  payload
 });
+
+// export const receiveMessages = (messages) => ({
+//   type: RECEIVE_MESSAGES,
+//   messages
+// });
 
 export const fetchMessage = (convId) => (dispatch) => (
   MessageUtil.fetchMessage(convId)
     .then(message => dispatch(receiveMessage(message)))
 );
+
+// export const fetchMessages = (convId) => (dispatch) => (
+//   MessageUtil.fetchMessages(convId)
+//     .then(messages => dispatch(receiveMessages(messages)))
+// );
