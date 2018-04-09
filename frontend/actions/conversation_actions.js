bca975058bcfb8de23ec58a0e5175e90beb066a2
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-09 10:13:17 -0700

    Create fetch message action

--- frontend/actions/conversation_actions.js
+++ frontend/actions/conversation_actions.js
@@ -1,4 +1,4 @@
-import * as APIUtil from '../util/conversation_api_util';
+import * as ConversationUtil from '../util/conversation_api_util';
 
 export const RECEIVE_ALL_CONVERSATIONS = 'RECEIVE_ALL_CONVERSATIONS';
 export const RECEIVE_CONVERSATION = 'RECEIVE_CONVERSATION';
@@ -20,31 +20,31 @@ export const receiveConversation = (payload) => ({
 // });
 
 export const fetchAllConversations = () => (dispatch) => (
-  APIUtil.fetchConversations()
+  ConversationUtil.fetchConversations()
     .then((payload) => dispatch(receiveAllConversations(payload)))
 );
 
 // export const fetchConversation = (id) => (dispatch) => (
-//   APIUtil.fetchConversation(id)
+//   ConversationUtil.fetchConversation(id)
 //     .then((conversation) => dispatch(receiveConversation(conversation)))
 // );
 
 export const fetchConversation = (id) => (dispatch) => (
-  APIUtil.fetchConversation(id)
+  ConversationUtil.fetchConversation(id)
     .then(conversation => dispatch(receiveConversation(conversation)))
 );
 
 export const createConversation = (convo) => (dispatch) => (
-  APIUtil.createConversation(convo)
+  ConversationUtil.createConversation(convo)
     .then((conversation) => dispatch(receiveConversation(conversation)))
 );
 
 // export const updateConversation = (convo) => (dispatch) => (
-//   APIUtil.updateConversation(convo)
+//   ConversationUtil.updateConversation(convo)
 //     .then((conversation) => dispatch(receiveConversation(conversation)))
 // );
 
 // export const deleteConversation = (conversationId) => (dispatch) => (
-//   APIUtil.deleteConversation(conversationId)
+//   ConversationUtil.deleteConversation(conversationId)
 //     .then((post) => dispatch(removeConversation(conversationId)))
 // );
--- frontend/actions/message_actions.js
+++ frontend/actions/message_actions.js
@@ -1,32 +1,19 @@
-export const ADD_MESSAGE = 'ADD_MESSAGE';
-export const SET_MESSAGES = 'SET_MESSAGES';
+import * as MessageUtil from '../util/message_api_util';
 
-export const addMessage = (message) => ({
-  type: ADD_MESSAGE,
-  message
-});
+export const SET_MESSAGES = 'SET_MESSAGES';
+export const RECEIVE_MESSAGE = 'RECEIVE_MESSAGE';
 
 export const setMessages = (messages) => ({
   type: SET_MESSAGES,
   messages
 });
 
-export const fetchMessage = (chatId) => ({
-  a: 'abc'
+export const receiveMessage = (message) => ({
+  type: RECEIVE_MESSAGE,
+  message
 });
 
-export const setUpChatRoom = (convId, action) => {
-  App.chatroom = App.cable.subscriptions.create({
-    channel: "ChatChannel",
-    conv_id: convId
-  }, {
-    connected: function() {},
-    disconnected: function() {},
-    received: function(data) {
-      return $('.live-chat').append(data['body']);
-    },
-    speak: function(message) {
-      return this.perform('speak', message);
-    }
-  });
-};
+export const fetchMessage = (convId) => (dispatch) => (
+  MessageUtil.fetchMessage(convId)
+    .then(message => dispatch(receiveMessage(message)))
+);
