5f1f640a5328f3de044f2d6e8cfc7a412518e7d3
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-08 23:27:10 -0700

    Add broadcast methods

--- app/jobs/message_creation_event_broadcast_job.rb
+++ app/jobs/message_creation_event_broadcast_job.rb
@@ -1,12 +1,30 @@
 class MessageCreationEventBroadcastJob < ApplicationJob
   queue_as :default
 
+  # def perform(message)
+  #   ActionCable
+  #     .server
+  #     .broadcast("chat_#{params[:id]}"),
+  #                id: message.id,
+  #                created_at: message.created_at.strftime('%H:%M'),
+  #                body: message.body,
+  #                sender_id: message.sender_id
+  #              )
+  # end
+
   def perform(message)
-    ActionCable
-      .server
-      .broadcast(Conversation.find(params[:id]),
-                 id: message.id,
-                 created_at: message.created_at.strftime('%H:%M'),
-                 content: message.body)
+    ActionCable.server.broadcast(
+      ("chat-#{message.conversation.id}"),
+      message: ActiveSupport::JSON.decode(render_message(message))
+    )
+  end
+
+  private
+
+  def render_message(message)
+    ApplicationController.render(
+      partial: 'api/messages/message',
+      locals: { message: message }
+    )
   end
 end
