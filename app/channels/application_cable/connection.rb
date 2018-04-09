9742605103e392b72cda0499dbaba211d9532ec6
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-08 23:16:30 -0700

    Remove excess code

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,18 +1,4 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
-
-    def connect
-    end
-
-    private
-
-    def find_current_user
-      if current_user = User.find_by(session[:session_token])
-        current_user
-      else
-        reject_unauthorized_connection
-      end
-    end
   end
 end
