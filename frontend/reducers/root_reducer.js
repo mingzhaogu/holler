21653ea4fcae52c39fb9adda7129c5578660a83a
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-09 23:32:37 -0700

    Add giphys reducer to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 // import each individual reducer:
 import EntitiesReducer from './entities_reducer';
-// import GiphysReducer from './giphys_reducer';
+import GiphysReducer from './giphys_reducer';
 import SessionReducer from './session_reducer';
 import ErrorsReducer from './errors_reducer';
 import UIReducer from './ui_reducer';
@@ -10,7 +10,7 @@ import UIReducer from './ui_reducer';
 // fix rootReducer to include actual reducers
 const rootReducer = combineReducers({
   entities: EntitiesReducer,
-
+  giphys: GiphysReducer,
   session: SessionReducer,
   errors: ErrorsReducer,
   ui: UIReducer
