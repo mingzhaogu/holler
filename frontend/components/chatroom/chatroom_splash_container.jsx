808cf956a34b7d023e1a5097b0d1683ec0c6afbe
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-11 22:17:59 -0700

    Clean up file

--- frontend/components/chatroom/chatroom_splash_container.jsx
+++ frontend/components/chatroom/chatroom_splash_container.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import Chatroom from './chatroom';
-// import { fetchConversation } from "../../../actions/conversation_actions";
 
 const mapStateToProps = (state, ownProps) => ({
   chatHead : 'Welcome!'
