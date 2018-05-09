5d977bf1ca2f34634dab36440eae7821037f2c1d
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-05-09 16:20:15 -0700

    Add message timestamp

--- frontend/components/messages/message_view_item.jsx
+++ frontend/components/messages/message_view_item.jsx
@@ -1,28 +1,89 @@
 import React from 'react';
 
-const MessageViewItem = ({ msg, currentUser, senderPic }) => {
-  let align;
-  if (msg.senderId === currentUser.id) align = "right";
-  else align = "left";
+// const MessageViewItem = ({ msg, currentUser, senderPic }) => {
+//   const timestamp = msg.timestamp;
+//
+//   const align = (msg.senderId === currentUser.id) ? "right" : "left";
+//
+//   let body;
+//   if (msg.messageType !== "text") {
+//     body = <img src={msg.body} />
+//   } else {
+//     body = msg.body
+//   }
+//
+//
+//   return (
+//     <li key={`chat-msg-${msg.id}`}
+//       className={`chat-msg chat-msg-align-${align}`}
+//     >
+//       <img src={senderPic} className="chat-msg-pic left" />
+//       <div className={chat-msg-timestamp left} />{timestamp}</div>
+//       <div className={`chat-msg-body chat-msg-body${align}`}>
+//         {body}
+//       </div>
+//       <img src={senderPic} className="chat-msg-pic right" />
+//     </li>
+//   )
+// }
 
-  let body;
-  if (msg.messageType !== "text") {
-    body = <img src={msg.body} />
-  } else {
-    body = msg.body
+// export default MessageViewItem;
+
+class MessageViewItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      displayTimestamp: false
+    }
+
+    this.changeTimestampDisplay = this.changeTimestampDisplay.bind(this)
   }
 
-  return (
-    <li key={`chat-msg-${msg.id}`}
-      className={`chat-msg chat-msg-align-${align}`}
-    >
-      <img src={senderPic} className="chat-msg-pic left" />
-      <div className={`chat-msg-body chat-msg-body${align}`}>
-        {body}
-      </div>
-      <img src={senderPic} className="chat-msg-pic right" />
-    </li>
-  )
+  body() {
+    const { msg } = this.props;
+    if (msg.messageType !== "text") {
+      return <img src={msg.body} />
+    } else {
+      return msg.body
+    }
+  }
+
+  changeTimestampDisplay(e) {
+    e.preventDefault();
+    this.setState({ displayTimestamp: !this.state.displayTimestamp })
+  }
+
+  render() {
+    const { msg, currentUser, senderPic } = this.props;
+    const timestamp = msg.timestamp;
+    const align = (msg.senderId === currentUser.id) ? "right" : "left";
+
+    const displayTimestamp = (this.state.displayTimestamp) ? "show" : "hide";
+    console.log(this.state.counter);
+
+    return (
+      <li key={`chat-msg-${msg.id}`}
+        className={`chat-msg chat-msg-align-${align}`}
+      >
+        <img src={senderPic} className="chat-msg-pic" />
+
+        <div className={`chat-msg-body chat-msg-body-${align}`}
+          onMouseOver={this.changeTimestampDisplay}
+          onMouseOut={this.changeTimestampDisplay}
+        >
+          {this.body()}
+        </div>
+
+        <div className={`chat-msg-timestamp ${displayTimestamp}`}>
+        {timestamp}
+        </div>
+      </li>
+    )
+  }
 }
+// <div className={`chat-msg-timestamp right ${displayTimestamp}`}>
+  // {timestamp}
+// </div>
+// <img src={senderPic} className="chat-msg-pic right" />
 
 export default MessageViewItem;
